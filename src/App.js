b343ec6fa5b928043f299ee6886bfbcc9031cb1a
searsyoung <sears.young@gmail.com>
2018-03-06 16:54:44 -0800

    Completed bloc-jams-router-checkpoint, typo corrected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/Landing";
-import Library from "./components/Libary";
+import Library from "./components/Library";
 
 class App extends Component {
   render() {
